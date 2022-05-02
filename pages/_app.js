397db23399191cf0e963b3e061cfcbbb93c617d9
pruthvivm44 <100123172+pruthvivm44@users.noreply.github.com>
2022-05-02 16:39:48 +0530

    integrated bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import "bootstrap/dist/css/bootstrap.css";
 import '../styles/globals.css';
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap");
+  }, []);
+  
   return <Component {...pageProps} />;
 }
 
